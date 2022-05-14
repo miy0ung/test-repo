408ecace731a31cb9e99d42492eca418de0797cc
miy0ung <101385210+miy0ung@users.noreply.github.com>
2022-05-14 17:59:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-김미영, 202100573, 컴퓨터공학부
-
-
-print("hello")
-print("Let me introduce myself. 김미영, 202100573, 컴퓨터공학부입니다")
-
-print('4주차 pull request 실습입니다!!')
+<font class="papago-parent"><font class="papago-source" style="display:none;">김미영, 202100573, 컴퓨터공학부
+</font>김미영, 202100573, 컴퓨터공학부</font><font class="papago-parent"><font class="papago-source" style="display:none;">
+</font>
+</font><font class="papago-parent"><font class="papago-source" style="display:none;">
+</font>
+</font><font class="papago-parent"><font class="papago-source" style="display:none;">print("hello")
+</font>print("hello")</font><font class="papago-parent"><font class="papago-source" style="display:none;">print("Let me introduce myself.</font>인쇄("내 소개를 할게.</font><font class="papago-parent"><font class="papago-source" style="display:none;"> 김미영, 202100573, 컴퓨터공학부입니다")
+</font>김미영, 202100573, 컴퓨터공학부")</font><font class="papago-parent"><font class="papago-source" style="display:none;">
+</font>
+</font><font class="papago-parent"><font class="papago-source" style="display:none;">print('4주차 pull request 실습입니다!!')</font>print('4주차 pull request 실습입니다!!')</font><font class="papago-parent"><font class="papago-source" style="display:none;">
+</font>
+</font>
