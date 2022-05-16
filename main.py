c3c1c3db2c8182cea2e6c44b23c125c393138b84
miy0ung <101385210+miy0ung@users.noreply.github.com>
2022-05-16 11:31:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-<font class="papago-parent"><font class="papago-source" style="display:none;">김미영, 202100573, 컴퓨터공학부
-</font>김미영, 202100573, 컴퓨터공학부</font><font class="papago-parent"><font class="papago-source" style="display:none;">
-</font>
-</font><font class="papago-parent"><font class="papago-source" style="display:none;">
-</font>
-</font><font class="papago-parent"><font class="papago-source" style="display:none;">print("hello")
-</font>print("hello")</font><font class="papago-parent"><font class="papago-source" style="display:none;">print("Let me introduce myself.</font>인쇄("내 소개를 할게.</font><font class="papago-parent"><font class="papago-source" style="display:none;"> 김미영, 202100573, 컴퓨터공학부입니다")
-</font>김미영, 202100573, 컴퓨터공학부")</font><font class="papago-parent"><font class="papago-source" style="display:none;">
-</font>
-</font><font class="papago-parent"><font class="papago-source" style="display:none;">print('4주차 pull request 실습입니다!!')</font>print('4주차 pull request 실습입니다!!')</font><font class="papago-parent"><font class="papago-source" style="display:none;">
-</font>
-</font>
+print(김미영, 202100573, 컴퓨터공학부)
+
+print(오픈소스 SW 및 실습 수업입니다)
